6a17e9a922a42c0b1a5b971640c60971e63595ff
ChauveSouris <desseobscurite@gmail.com>
2023-10-05 01:03:48 +0500

    Initial Commit

---
+++ AnimationApp/AnimationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationAppApp.swift
+//  AnimationApp
+//
+//  Created by Елизавета Медведева on 05.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct AnimationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
